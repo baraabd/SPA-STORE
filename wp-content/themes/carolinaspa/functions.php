54da58f69d8ae64d66848839ae0fea97c8f32cd2
Afshar20922019 <afshar2092@yahoo.com>
2020-05-04 01:54:35 +0200

    Just Released products fix

--- wp-content/themes/carolinaspa/functions.php
+++ wp-content/themes/carolinaspa/functions.php
@@ -1,4 +1,16 @@
 <?php
+function carolinaspa_new_products($args){
+    $args ['limit']   = 6;
+    $args ['columns']   = 3;
+    $args ['title']   = 'Just Released';
+
+return $args;
+
+}
+add_filter('storefront_recent_products_args','carolinaspa_new_products');
+
+
+
 
 
 function carolinaspa_setup() {
