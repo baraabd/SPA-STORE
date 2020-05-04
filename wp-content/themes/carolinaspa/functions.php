c5ac0f5ecb5c0dfb5534a7090fe26bab433192f5
Bara Abdullatif <bara.abd@yahoo.com>
2020-05-04 18:21:09 +0200

    Display Home Kits in the Homepage

--- wp-content/themes/carolinaspa/functions.php
+++ wp-content/themes/carolinaspa/functions.php
@@ -52,3 +52,29 @@ remove_action('woocommerce_single_product_summary', 'woocommerce_template_single
 add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 35);
 
 
+// Display Home Kits in the Homepage
+
+function carolinaspa_homepage_homekits()
+{ ?>
+
+    <div class="homepage-home-kit-category">
+        <div class="content">
+            <div class="columns-3">
+
+                <?php $home_kit = get_term(18, 'product_cat', ARRAY_A); ?>
+                <h2 class="section-title"><?php echo $home_kit['name']; ?></h2>
+                <p><?php echo $home_kit['description']; ?></p>
+                <a href="<?php echo get_category_link($home_kit['term_id']); ?>">
+                        All Products &raquo;
+                    </a>
+            </div>
+            <?php echo do_shortcode('[product_category category="home-kits" per_page="3" orderby="price" order="asc" columns="9" ]'); ?>
+
+        </div>
+    </div>
+<?php
+
+}
+
+add_action('homepage', 'carolinaspa_homepage_homekits', 23);
+
