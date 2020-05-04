6d58a26510606dfc0bb5236115662649c3f1219b
Bara Abdullatif <bara.abd@yahoo.com>
2020-05-04 18:19:48 +0200

    Add style shits and scripts

--- wp-content/themes/carolinaspa/functions.php
+++ wp-content/themes/carolinaspa/functions.php
@@ -8,3 +8,30 @@ return $args;
 
 }
 add_filter('storefront_recent_products_args','carolinaspa_new_products');
+
+
+
+function carolinaspa_setup() {
+    add_image_size('blog_entry', 400, 257, true);
+}
+add_action('after_setup_theme', 'carolinaspa_setup');
+
+
+//Add styleshits and scripts
+
+function spastore_scripts()
+{
+
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Lato:400,700,900|Lora:400,700');
+}
+add_action('wp_enqueue_scripts', 'spastore_scripts');
+
+
+
+
+
+
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price');
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 35);
+
+
